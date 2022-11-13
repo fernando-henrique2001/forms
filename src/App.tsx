9226eb301961c10c1cbd9f5349cbad67e7550f51
fernando-henrique <fernando.henrique@rethink.dev>
2022-11-13 11:24:18 -0300

    Adicionando logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import styles from "./App.module.css";
 import Form from "./component/Form";
+import logo from "./assets/FSMIrJCWQAQTYgt.jpg";
 // import logo from "logo.svg";
 
 function App() {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <div className={styles.logo}></div>
+        <div className={styles.logo}>
+          <div className={styles.title}>
+            {/* <h1>Programa de</h1>
+            <h2>Estágio</h2>
+            <h3>2023</h3> */}
+            <img src={logo} alt="" />
+          </div>
+        </div>
 
         <div className={styles.overlay}></div>
       </div>
